9d25b0a9c6d311e88ecdbbc928b0bd24430d9f50
lePossum <batjabentli@mail.ru>
2019-03-30 14:24:15 +0300

    kind of 4th task (with clang)

--- src/network/st_nonblocking/Connection.h
+++ src/network/st_nonblocking/Connection.h
@@ -3,63 +3,65 @@
 
 #include <cstring>
 #include <iostream>
-#include <vector>
 #include <string>
 #include <sys/uio.h>
+#include <vector>
 
-#include <sys/epoll.h>
-#include <afina/execute/Command.h>
-#include <afina/Storage.h>
 #include "protocol/Parser.h"
+#include <afina/Storage.h>
+#include <afina/execute/Command.h>
 #include <spdlog/logger.h>
+#include <sys/epoll.h>
 
 namespace Afina {
 namespace Network {
-namespace STnonblock {
+    namespace STnonblock {
 
-class Connection {
-public:
-    Connection(int s) : _socket(s) {
-        std::memset(&_event, 0, sizeof(struct epoll_event));
-        _event.data.ptr = this;
-    }
+        class Connection {
+        public:
+            Connection(int s)
+                : _socket(s)
+            {
+                std::memset(&_event, 0, sizeof(struct epoll_event));
+                _event.data.ptr = this;
+            }
 
-    inline bool isAlive() const { return _alive; }
+            inline bool isAlive() const { return _alive; }
 
-    void Start(std::shared_ptr<spdlog::logger> logger);
+            void Start(std::shared_ptr<spdlog::logger> logger);
 
-protected:
-    void OnError();
-    void OnClose();
-    void DoRead();
-    void DoWrite();
+        protected:
+            void OnError();
+            void OnClose();
+            void DoRead();
+            void DoWrite();
 
-private:
-    friend class ServerImpl;
+        private:
+            friend class ServerImpl;
 
-    static const int mask_r = EPOLLIN | EPOLLRDHUP | EPOLLERR;
-    static const int mask_rw = EPOLLIN | EPOLLRDHUP | EPOLLERR | EPOLLOUT;
+            static const int mask_r = EPOLLIN | EPOLLRDHUP | EPOLLERR;
+            static const int mask_rw = EPOLLIN | EPOLLRDHUP | EPOLLERR | EPOLLOUT;
 
-    std::shared_ptr<spdlog::logger> _logger;
-    std::shared_ptr<Afina::Storage> pStorage;
+            std::shared_ptr<spdlog::logger> _logger;
+            std::shared_ptr<Afina::Storage> pStorage;
 
-    int _socket;
-    bool _alive; //
-    struct epoll_event _event;
+            int _socket;
+            bool _alive; //
+            struct epoll_event _event;
 
-    std::size_t arg_remains;
-    Protocol::Parser parser;
-    std::string argument_for_command;
-    std::unique_ptr<Execute::Command> command_to_execute;
+            std::size_t arg_remains;
+            Protocol::Parser parser;
+            std::string argument_for_command;
+            std::unique_ptr<Execute::Command> command_to_execute;
 
-    int read_bytes = 0;
-    char client_buffer[4096];
+            int read_bytes = 0;
+            char client_buffer[4096];
 
-    std::vector<std::string> _answers;
-    int _position = 0;
-};
+            std::vector<std::string> _answers;
+            int _position = 0;
+        };
 
-} // namespace STnonblock
+    } // namespace STnonblock
 } // namespace Network
 } // namespace Afina
 
